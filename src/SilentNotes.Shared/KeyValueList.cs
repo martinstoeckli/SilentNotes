cec84306bc83bba2c1be3e99ab9b6a786a6a8cca
Martin Stoeckli <developer@martinstoeckli.ch>
2019-01-22 14:41:02 +0100

    Updated license for commonly used classes.

--- src/SilentNotes.Shared/KeyValueList.cs
+++ src/SilentNotes.Shared/KeyValueList.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright © 2018 Martin Stoeckli.
-// This Source Code Form is subject to the terms of the Mozilla Public
-// License, v. 2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the license was not distributed with this
+// file, You can obtain one at https://opensource.org/licenses/MIT.
 
 using System.Collections.Generic;
 using System.Diagnostics;
