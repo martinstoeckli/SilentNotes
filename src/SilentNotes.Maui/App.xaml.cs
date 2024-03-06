224c4f46f83ae69fcff52df5f905d1a960a44a24
Martin Stoeckli <developer@martinstoeckli.ch>
2024-03-06 22:59:31 +0100

    SilentNotes.Maui minimum version.

---
+++ src/SilentNotes.Maui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SilentNotes
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ src/SilentNotes.Maui/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SilentNotes
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
