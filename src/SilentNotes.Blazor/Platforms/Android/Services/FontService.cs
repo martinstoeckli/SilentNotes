674a2ecd3d04b0b677a5471d12f9886565208859
Martin Stoeckli <developer@martinstoeckli.ch>
2026-01-12 14:47:39 +0100

    Improved startup time by eliminating external libraries.

--- src/SilentNotes.Blazor/Platforms/Android/Services/FontService.cs
+++ src/SilentNotes.Blazor/Platforms/Android/Services/FontService.cs
@@ -4,8 +4,8 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 using System.Collections.Generic;
+using System.Xml.Linq;
 using SilentNotes.Services;
-using SkiaSharp;
 
 namespace SilentNotes.Platforms.Services
 {
@@ -14,12 +14,39 @@ namespace SilentNotes.Platforms.Services
     /// </summary>
     public class FontService : IFontService
     {
-        /// <inheritdoc/>
+        /// <summary>
+        /// Gets a sorted list of font families available on the running OS.
+        /// </summary>
+        /// <remarks>
+        /// Android, since OperatingSystem.IsAndroidVersionAtLeast(29), allows to list the system
+        /// font files via SystemFonts.AvailableFonts, but unfortunately a WebView cannot access
+        /// and use them to display text.
+        /// Therefore, in lack of an alternative, we do the same a the SkiaSharp library does to
+        /// get the "SKFontManager.Default.FontFamilies", we parse Androids fonts.xml file.
+        /// </remarks>
+        /// <returns>Enumeration of font family names.</returns>
         public List<string> ListFontFamilies()
         {
-            List<string> result = SKFontManager.Default.FontFamilies.ToList();
-            result.Sort(StringComparer.InvariantCultureIgnoreCase);
-            return result;
+            try
+            {
+                var result = new List<string>();
+                var xml = XDocument.Load(@"/system/etc/fonts.xml");
+
+                var familyElements = xml.Root.Descendants("family");
+                var familyNames = familyElements.Select(element => element.Attribute("name")?.Value).Where(name => !string.IsNullOrEmpty(name));
+                result.AddRange(familyNames);
+
+                var aliasElements = xml.Root.Descendants("alias");
+                var aliasNames = aliasElements.Select(element => element.Attribute("name")?.Value).Where(name => !string.IsNullOrEmpty(name));
+                result.AddRange(aliasNames);
+
+                result.Sort();
+                return result;
+            }
+            catch (Exception)
+            {
+                return new List<string>();
+            }
         }
     }
 }
